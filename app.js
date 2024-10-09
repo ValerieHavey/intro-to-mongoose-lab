7c5070de3b227bb8b59b5944f09d56f21a1fb8a4
Valeriehavey-ga <valeriehavey@gmail.com>
2024-10-09 11:38:55 -0700

    completed

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const prompt = require("prompt-sync")();
 mongoose.connect(process.env.MONGODB_URI);
 
 const selectOperation = () => {
-  console.log("1. Create");
-  console.log("2. Read");
-  console.log("3. Update");
-  console.log("4. Delete");
+  console.log("1. Create a customer");
+  console.log("2. View all customers");
+  console.log("3. Update a customer");
+  console.log("4. Delete a customer");
   console.log("5. Quit");
 
   const userOption = prompt("Number of action to run:");
@@ -18,7 +18,14 @@ const selectOperation = () => {
 
   if (userOption === "1") {
     createOperation();
+  } else if (userOption === "2") {
+    readOperation();
+  } else if (userOption === "3") {
+    updateOperation(); 
+  } else if (userOption === "4") {
+    deleteOperation();
   } else if (userOption === "5") {
+    mongoose.connection.close();
     process.exit(0);
   } else {
     selectOperation();
@@ -26,7 +33,7 @@ const selectOperation = () => {
 };
 
 const createOperation = async () => {
-  console.log("create");
+  console.log("Create a customer");
   const name = prompt("What is the customers name?");
   const age = prompt("What is the customers age?");
   await customerModel.create({
@@ -37,13 +44,40 @@ const createOperation = async () => {
 };
 
 
+const readOperation = async () => {
+    console.log("Below is a list of customers");
+    const listCustomers = await customerModel.find({}); 
+    listCustomers.forEach((customer) => {
+      console.log(`id: ${customer.id} --- Name: ${customer.name}, Age: ${customer.age}`);
+    })
+    selectOperation();
+};
+
+const updateOperation = async () => {
+    console.log("update");
+    const listCustomers = await customerModel.find({}); 
+    listCustomers.forEach((customer) => {
+      console.log(`id: ${customer.id} --- Name: ${customer.name}, Age: ${customer.age}`);
+    });
+    const id = prompt("What is the id of the customer you want to update?");
+    const name = prompt("What is the customers new name?");
+    const age = prompt("What is the customers new age?");
+    await customerModel.findByIdAndUpdate(id, {
+      name: name,
+      age: age,
+    });
+    selectOperation();
+  };
+
+const deleteOperation = async () => {
+    console.log("delete");
+    const listCustomers = await customerModel.find({}); 
+    listCustomers.forEach((customer) => {
+      console.log(`id: ${customer.id} --- Name: ${customer.name}, Age: ${customer.age}`);
+    });
+    const id = prompt("What is the id of the customer you want to delete?");
+    await customerModel.findByIdAndDelete(id);
+    selectOperation();
+}
 
 selectOperation();
-
-// const createCustomerModel = async ()  => {
-//     const customerModelData = {
-//         name: "Customer Model",
-//         age:
-//     };
-//     const
-// }
